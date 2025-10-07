548544439290fa14a9d5b21999ac421f945e2c4d
Chirag Rai <chiragrai894@gmail.com>
2025-10-07 21:53:35 +0530

    Add files via upload

--- singlylinked.java
+++ singlylinked.java
@@ -1,22 +1,42 @@
 public class singlylinked{
-    private Listnode head;
+    public listnode head;
 
-    private static class Listnode{
-        private int data;
-        private Listnode next;
-        
-        public Listnode(int data){
-            this.data=data;
-            this.next=null;
-        }
+    public static class listnode{
+    public int data;
+    public listnode next;
+
+    public listnode(int data){
+        this.data=data;
+        this.next=null;
     }
-    public static void main(String[] args){
+}
+public void display(){
+    listnode current=head;
+    while(current !=null){
+       System.out.println(current.data+ " ");
+       current=current.next;    
+    }
+}
+public int count(){
+    int count=0;
+    listnode current=head;
+    while(current!=null){
+        count++;
+        current=current.next;
+    }
+    return count;
+}
+public static void main(String[] args){
     singlylinked sll=new singlylinked();
-    sll.head=new Listnode(10);
-    Listnode second= new Listnode(1);
-    Listnode third= new Listnode(8);
-   Listnode fourth=new Listnode(11);
+    sll.head=new listnode(10);
+    listnode second=new listnode(20);
+    listnode third=new listnode(30);
+    listnode fourth=new listnode(40);
+    sll.head.next=second;
+    second.next=third;
 
-   sll.head=  
-    }
+     sll.display();
+     System.out.println("The length is" + sll.count());
+
+ }
 }
